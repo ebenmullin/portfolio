8f7d84b3cc7325810267a4f32b4ea1f6258c2792
BuildTools <unconfigured@null.spigotmc.org>
2022-04-27 20:35:27 +1200

    added marquee text and fixed portfolio animation

--- index.js
+++ index.js
@@ -49,4 +49,50 @@ tl
   .to("nav", {opacity: 1, y: 0, duration: 0.3})
   .to("h1", {opacity: 1, y: 0, duration: 1})
   // .to(".section-landing", {width: 95 + "%", height: 95 + "vh", opacity: 1, "border-radius": 25, duration: 2})
-  .to("h3", {"clip-path": "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)", opacity: 1, x: 0, duration: 2})
+
+
+
+
+
+
+
+
+var rollingTween = new TimelineMax();
+var time = 20;
+var $rollingTextgoLeft = $(".rolling-text");
+var $text = $(".text")
+
+function startRolling() {
+
+  $text.css({width:"auto"});
+  $text.width(width);
+  var width = $text.width();
+  
+
+  rollingTween.to(".rolling-text", time, {
+    x: -width,
+    ease: Linear.easeNone,
+    repeat: -1
+  })
+
+  return rollingTween;
+}
+
+function rollingText(){
+  $('.rolling-text').clone().appendTo(".wrapper-rolling-text");
+  startRolling();   
+}
+rollingText();
+
+function resizeCheck(){
+  var progress = rollingTween.progress(); //record the progress so that we can match it with the new tween (jump to the same spot)
+  rollingTween.time(0).kill(); //rewind and kill the original tween.
+  //time = 5;
+  
+  startRolling().progress(progress); //create a new tween based on the new size, and jump to the same progress value.
+};
+
+resizeCheck();
+$(window).resize(resizeCheck);
+
+
