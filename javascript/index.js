d3971d31b96ffb3f07742b8154e669087b8c3b22
BuildTools <unconfigured@null.spigotmc.org>
2022-08-08 11:40:15 +1200

    changed contact form, fixed mobile nav

--- javascript/index.js
+++ javascript/index.js
@@ -1,29 +1,67 @@
 gsap.registerPlugin(ScrollTrigger);
 
-let masks = document.querySelectorAll(".mask");
 
 $(document).ready(function(){
   $(this).scrollTop(0);
 });
 
-$(".toggle-button").click(function() {
-  $(".nav-links").toggleClass("active");
-})
+$(".hamburger").click(function() {
+  navExpanded.reversed() ? navExpanded.play() : navExpanded.reverse();
+});
+
+$(".nav-link ul li").click(function() {
+  console.log("working")
+});
 
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
-
+  
   if(scroll >= 10) {
     $("nav").addClass("expanded");
-
+    
   } else {
     $("nav").removeClass("expanded");
   }
-
+  
 });
+
+
+let navExpanded = gsap.timeline({paused:true, reversed:true});
+
+
+navExpanded
+  .to($(".hamburger .bar:nth-child(1)"), 0.3, {y: 8, rotation: 45}, "start")
+  .to($(".hamburger .bar:nth-child(2)"), 0.3, {opacity: 0}, "start")
+  .to($(".hamburger .bar:nth-child(3)"), 0.3, {y: -8, rotation: -45}, "start")
+
+  .to($(".nav-links ul"), {duration: 1, ease: "power1.out", "clip-path": "circle(150% at 100% 0%)"}, "start")
+  .staggerFrom($(".nav-links li, .nav-links button"), 0.5, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=0.3")
+
+// tried to turn standard js code into jQuery code
+
+// let masks = $(".mask")
+
+// masks.each(function() {
+//   mask = this;
+//   let image = $(mask).find("img");
+//   console.log(image);
+//   let iframe = $(mask) + $("iframe");
+//   console.log(iframe);
+
+//   let tl = gsap.timeline({
+//     scrollTrigger: {trigger: mask, toggleActions: "restart none none reset"}
+//   });
+  
+//   tl.from(mask, 1.5, {xPercent: -100, ease: Power2.out});
+//   tl.from(image, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
+//   tl.from(iframe, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
+// })
+
+let masks = document.querySelectorAll(".mask");
 
 masks.forEach(mask => {
   let image = mask.querySelector("img");
+  console.log(image);
   let iframe = mask.querySelector("iframe");
   
   let tl = gsap.timeline({
@@ -36,27 +74,23 @@ masks.forEach(mask => {
   tl.from(iframe, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
 })
 
-let tl = gsap.timeline({
-  defaults: {ease: "power4.inOut", duration: 2}
-});
+// Pointless gsap loading animations
 
-// let contactTransition = gsap.timeline({paused: true});
-  
-
-tl
-  .set("html", {"overflow": "hidden"})
-  .to(".intro", {opacity: 1, y: 0, duration: 1, stagger: .2})
-  .to(".overlay", {height: 0, display: "none", duration: 1, ease: "power1.in"})
-  .set("html", {"overflow-y": "auto"})
-  .to("nav", {opacity: 1, y: 0, duration: 0.5})
-  // .from(".section-landing", {width: 100 + "%", height: 100 + "vh", "border-radius": 0, margin: 0})
-  .to(".welcome", {opacity: 1, y: 0, duration: 1})
-
-// contactTransition
-//   .to(".section-contact", {x: 0 + "%", duration: 1})
-// $(".contact").click(function() {
-//   contactTransition.play(0);
+// let tl = gsap.timeline({
+//   defaults: {ease: "power4.inOut", duration: 2}
 // });
+
+// tl
+//   .set("html", {"overflow": "hidden"})
+//   .to(".intro", {opacity: 1, y: 0, duration: 1, stagger: .2})
+//   .to(".overlay", {height: 0, display: "none", duration: 1, ease: "power1.in"})
+//   .set("html", {"overflow-y": "auto"})
+//   .to("nav", {opacity: 1, y: 0, duration: 0.5})
+//   // .from(".section-landing", {width: 100 + "%", height: 100 + "vh", "border-radius": 0, margin: 0})
+//   .to(".welcome", {opacity: 1, y: 0, duration: 1})
+
+
+// Homepage reactive Mouse
 
 $(document).mousemove(function(event) {
   var xPos = (event.clientX / $(window).width())-0.5,
@@ -72,47 +106,7 @@ $(document).mousemove(function(event) {
  });
 });
 
-
-// var cursor = document.querySelector('.cursor');
-
-// document.addEventListener('mousemove', function(e){
-//   var x = e.clientX;
-//   var y = e.clientY;
-//   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`
-// });
-
-// document.addEventListener('mousemove', function(e){
-//   var x = e.clientX;
-//   var y = e.clientY;
-// });
-
-// document.addEventListener('mousedown', function(){
-//   cursor.classList.add('click');
-// });
-
-// document.addEventListener('mouseup', function(){
-//   cursor.classList.remove('click')
-// });
-
-
-              // Plays video on scroll
-
-// const videoUrl = "https://www.youtube.com/embed/mwpSjEGBJV8?controls=0&loop=1&playlist=mwpSjEGBJV8";
-
-// const observer = new IntersectionObserver(elements => {
-//   if(elements[0].intersectionRatio > 0) {
-//     console.log("The element is in view!");
-//     $("#player").attr("src", videoUrl + "&autoplay=1");
-//   }else {
-//     console.log("The element is out of view")
-//     $("#player").attr("src", videoUrl);
-
-//   }
-// })
-
-// const iframe = $("iframe")[0];
-
-// observer.observe(iframe);
+// Changes Isometric Image on hover
 
 $("#isometric").hover(function() {
   $("#isometric").attr("src", "assets/isometric-2.png");
@@ -120,6 +114,9 @@ $("#isometric").hover(function() {
 }, function() {
   $("#isometric").attr("src", "assets/isometric-1.png");
 });
+
+
+// Modal for portfolio
 
 const modal = $(".modal");
 const previews = $(".image-box img");
