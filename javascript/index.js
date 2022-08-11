72cb1216d92ec685a84fd0a7733e83a9285fdfba
BuildTools <unconfigured@null.spigotmc.org>
2022-08-12 08:45:21 +1200

    complete landing page overhall

--- javascript/index.js
+++ javascript/index.js
@@ -1,10 +1,3 @@
-gsap.registerPlugin(ScrollTrigger);
-
-
-$(document).ready(function(){
-  $(this).scrollTop(0);
-});
-
 $(".hamburger").click(function() {
   navExpanded.reversed() ? navExpanded.play() : navExpanded.reverse();
 });
@@ -13,29 +6,20 @@ $(".nav-link ul li").click(function() {
   console.log("working")
 });
 
-$(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-  
-  if(scroll >= 10) {
-    $("nav").addClass("expanded");
-    
-  } else {
-    $("nav").removeClass("expanded");
-  }
-  
-});
+let navExpanded = gsap.timeline({paused: true});
+
+// navExpanded
+//   .to($(".hamburger .bar:nth-child(1)"), 0.3, {y: 8, rotation: 45}, "start")
+//   .to($(".hamburger .bar:nth-child(2)"), 0.3, {opacity: 0}, "start")
+//   .to($(".hamburger .bar:nth-child(3)"), 0.3, {y: -8, rotation: -45}, "start")
+
+//   .to($(".nav-links ul"), {duration: 1, ease: "power1.out", "clip-path": "circle(150% at 100% 0%)"}, "start")
+//   .staggerFrom($(".nav-links li, .nav-links button"), 0.5, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=0.3")
 
 
-let navExpanded = gsap.timeline({paused:true, reversed:true});
+// observer = new IntersectionObserver;
 
 
-navExpanded
-  .to($(".hamburger .bar:nth-child(1)"), 0.3, {y: 8, rotation: 45}, "start")
-  .to($(".hamburger .bar:nth-child(2)"), 0.3, {opacity: 0}, "start")
-  .to($(".hamburger .bar:nth-child(3)"), 0.3, {y: -8, rotation: -45}, "start")
-
-  .to($(".nav-links ul"), {duration: 1, ease: "power1.out", "clip-path": "circle(150% at 100% 0%)"}, "start")
-  .staggerFrom($(".nav-links li, .nav-links button"), 0.5, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=0.3")
 
 // tried to turn standard js code into jQuery code
 
@@ -68,38 +52,32 @@ masks.forEach(mask => {
     scrollTrigger: {trigger: mask, toggleActions: "restart none none reset"}
   });
 
-  tl.from(mask, 1.5, {xPercent: -100, ease: Power2.out});
-
-  tl.from(image, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
-  tl.from(iframe, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
+  tl
+    .from(mask, 1.5, {xPercent: -100, ease: Power2.out})
+    .from(image, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out})
+    .from(iframe, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
 })
 
-// Pointless gsap loading animations
 
-// let tl = gsap.timeline({
-//   defaults: {ease: "power4.inOut", duration: 2}
-// });
 
-// tl
-//   .set("html", {"overflow": "hidden"})
-//   .to(".intro", {opacity: 1, y: 0, duration: 1, stagger: .2})
-//   .to(".overlay", {height: 0, display: "none", duration: 1, ease: "power1.in"})
-//   .set("html", {"overflow-y": "auto"})
-//   .to("nav", {opacity: 1, y: 0, duration: 0.5})
-//   // .from(".section-landing", {width: 100 + "%", height: 100 + "vh", "border-radius": 0, margin: 0})
-//   .to(".welcome", {opacity: 1, y: 0, duration: 1})
+let tl = gsap.timeline({
+  defaults: {ease: "power4.inOut", duration: 2}
+});
 
+tl
+  .from($(".section-landing"), 1, {"background-image": "linear-gradient(90deg, #b3b4af 0%, #1a1a1d 0%)"})
+  .to($(".hero"), 1, {width: "50%"}, "-=0.5")
 
 // Homepage reactive Mouse
 
 $(document).mousemove(function(event) {
   var xPos = (event.clientX / $(window).width())-0.5,
       yPos = (event.clientY/$(window).height())-0.5,
-      plane = $(".section-landing");
+      plane = $(".hero");
  
  TweenLite.to(plane, 0.5, {
-   rotationY: 5 * xPos, 
-   rotationX: 5 * yPos,
+   rotationY: 10 * xPos, 
+   rotationX: 10 * yPos,
    ease: "Power4.inOut",
    transformPerspective: 1000,
    transformOrigin: "center"
@@ -140,4 +118,10 @@ modal.on("click", (e) => {
     original.removeClass("open");
 
   }
+});
+
+// apply parallax effect to any element with a data-speed attribute
+gsap.to("[data-speed]", {
+  y: (i, el) => (1 - parseFloat(el.getAttribute("data-speed"))) * ScrollTrigger.maxScroll(window), ease: "none",
+  scrollTrigger: {start: 0, end: "max", invalidateOnRefresh: true, scrub: 0}
 });
