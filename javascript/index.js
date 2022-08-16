be99e29518a63d5ca3fd622bbe64636bbf0d8c8e
BuildTools <unconfigured@null.spigotmc.org>
2022-08-17 11:02:17 +1200

    updated website hero

--- javascript/index.js
+++ javascript/index.js
@@ -6,7 +6,7 @@ $(".nav-link ul li").click(function() {
   console.log("working")
 });
 
-let navExpanded = gsap.timeline({paused: true});
+// let navExpanded = gsap.timeline({paused: true});
 
 // navExpanded
 //   .to($(".hamburger .bar:nth-child(1)"), 0.3, {y: 8, rotation: 45}, "start")
@@ -16,68 +16,66 @@ let navExpanded = gsap.timeline({paused: true});
 //   .to($(".nav-links ul"), {duration: 1, ease: "power1.out", "clip-path": "circle(150% at 100% 0%)"}, "start")
 //   .staggerFrom($(".nav-links li, .nav-links button"), 0.5, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=0.3")
 
-
-// observer = new IntersectionObserver;
-
-
-
 // tried to turn standard js code into jQuery code
 
-// let masks = $(".mask")
+let Jmasks = $(".mask");
 
-// masks.each(function() {
-//   mask = this;
-//   let image = $(mask).find("img");
-//   console.log(image);
-//   let iframe = $(mask) + $("iframe");
-//   console.log(iframe);
+Jmasks.each(Jmask => {
+  let Jimage = $(Jmask).find("img")
+  console.log(Jimage + "this is cool");
+  let Jiframe = $(Jmask).find("iframe")
+  
+  let tl = gsap.timeline({
+    scrollTrigger: {trigger: Jmask, toggleActions: "restart none none reset"}
+  });
+  
+  tl
+  .from(Jmask, 1.5, {xPercent: -100, ease: Power2.out})
+  .from(Jimage, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out})
+  .from(Jiframe, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
+})
 
+// let masks = document.querySelectorAll(".mask");
+
+// masks.forEach(mask => {
+//   let image = mask.querySelector("img");
+//   console.log(image + "testing testing");
+//   let iframe = mask.querySelector("iframe");
+  
 //   let tl = gsap.timeline({
 //     scrollTrigger: {trigger: mask, toggleActions: "restart none none reset"}
 //   });
-  
-//   tl.from(mask, 1.5, {xPercent: -100, ease: Power2.out});
-//   tl.from(image, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
-//   tl.from(iframe, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
-// })
 
-let masks = document.querySelectorAll(".mask");
-
-masks.forEach(mask => {
-  let image = mask.querySelector("img");
-  console.log(image);
-  let iframe = mask.querySelector("iframe");
-  
-  let tl = gsap.timeline({
-    scrollTrigger: {trigger: mask, toggleActions: "restart none none reset"}
-  });
-
-  tl
-    .from(mask, 1.5, {xPercent: -100, ease: Power2.out})
-    .from(image, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out})
-    .from(iframe, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
-})
+//   tl
+//     .from(mask, 1.5, {xPercent: -100, ease: Power2.out})
+//     .from(image, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out})
+//     .from(iframe, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
+// });
 
 
 
 let tl = gsap.timeline({
-  defaults: {ease: "power4.inOut", duration: 2}
+  defaults: {ease: "power2.easeInOut", duration: 2}
 });
 
 tl
-  .from($(".section-landing"), 1, {"background-image": "linear-gradient(90deg, #b3b4af 0%, #1a1a1d 0%)"})
-  .to($(".hero"), 1, {width: "50%"}, "-=0.5")
-
+  .from($(".section-landing"), 1, {"background-image": "linear-gradient(90deg, #b3b4af 0%, #1a1a1d 0%)"}, "start")
+  .to($("nav"), 2, {opacity: 1}, "start")
+  .from($(".hero"), 2, {xPercent: -130}, "start")
+  .from($(".hero img"), 2, {xPercent: 130}, "start")
+  .from($(".text-container"), 1, {opacity: 0, yPercent: 20}, "=-1")
 // Homepage reactive Mouse
 
 $(document).mousemove(function(event) {
   var xPos = (event.clientX / $(window).width())-0.5,
-      yPos = (event.clientY/$(window).height())-0.5,
+      yPos = (event.clientY / $(window).height())-0.5,
       plane = $(".hero");
  
  TweenLite.to(plane, 0.5, {
-   rotationY: 10 * xPos, 
-   rotationX: 10 * yPos,
+   rotationY: 100 * xPos, 
+   rotationX: -100 * yPos,
+   x: -200 * xPos,
+   y: -200 * yPos,
    ease: "Power4.inOut",
    transformPerspective: 1000,
    transformOrigin: "center"
@@ -96,10 +94,10 @@ $("#isometric").hover(function() {
 
 // Modal for portfolio
 
-const modal = $(".modal");
-const previews = $(".image-box img");
-const original = $(".modal-img");
-const caption = $(".caption");
+const modal = $(".modal"),
+      previews = $(".image-box img"),
+      original = $(".modal-img"),
+      caption = $(".caption");
 
 previews.each(function(preview) {
   $(this).on("click", () => {
@@ -125,3 +123,12 @@ gsap.to("[data-speed]", {
   y: (i, el) => (1 - parseFloat(el.getAttribute("data-speed"))) * ScrollTrigger.maxScroll(window), ease: "none",
   scrollTrigger: {start: 0, end: "max", invalidateOnRefresh: true, scrub: 0}
 });
+
+
+const target = document.querySelector(".section-landing");
+
+const observer = new IntersectionObserver(() => {
+    $("nav").toggleClass("expanded")
+});
+
+observer.observe(target);
