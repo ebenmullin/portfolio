cc33f490d5c324c04d8c2a1f297d33792b20e564
BuildTools <unconfigured@null.spigotmc.org>
2024-05-03 10:56:55 +1200

    fixed burger menu, made optimizations

--- javascript/index.js
+++ javascript/index.js
@@ -1,18 +1,22 @@
-let navExpanded;
+let navExpanded = gsap.timeline({ paused: true });
 
+// Define the animation sequence
+navExpanded
+  .to($(".hamburger .bar:nth-child(1)"), 0.3, { y: 8, rotation: 45 }, "start")
+  .to($(".hamburger .bar:nth-child(2)"), 0.3, { opacity: 0 }, "start")
+  .to($(".hamburger .bar:nth-child(3)"), 0.3, { y: -8, rotation: -45 }, "start")
+  .to($(".nav-links .wrapper"), { duration: 1, ease: "power1.out", "clip-path": "circle(150% at 100% 0%)" }, "start")
+  .staggerTo($(".nav-links li, .nav-links button"), 0.5, { opacity: 1, y: 0, ease: Power1.easeOut }, 0.1, "-=0.3");
+
+// Toggle the navigation animation on hamburger click
 $(".hamburger").click(function() {
   navExpanded.reversed() ? navExpanded.play() : navExpanded.reverse();
 });
 
-navExpanded = gsap.timeline({paused: true});
-
-navExpanded
-  .to($(".hamburger .bar:nth-child(1)"), 0.3, {y: 8, rotation: 45}, "start")
-  .to($(".hamburger .bar:nth-child(2)"), 0.3, {opacity: 0}, "start")
-  .to($(".hamburger .bar:nth-child(3)"), 0.3, {y: -8, rotation: -45}, "start")
-  .to($(".nav-links .wrapper"), {duration: 1, ease: "power1.out", "clip-path": "circle(150% at 100% 0%)"}, "start")
-  .staggerTo($(".nav-links li, .nav-links button"), 0.5, {opacity: 1, y: 0, ease: Power1.easeOut}, 0.1, "-=0.3");
-  // .staggerFrom($(".nav-links li, .nav-links button"), 0.5, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=0.3");
+// Close the navigation when a link is clicked
+$(".nav-links a").click(function() {
+  navExpanded.reverse();
+});
 
 let masks = document.querySelectorAll(".mask");
 
@@ -30,12 +34,12 @@ masks.forEach(mask => {
     .from(video, 1.5, {xPercent: 100, scale: 1.3, delay: -1.5, ease: Power2.out});
 });
 
-let counter = {
-  value: 0
-}
+// let counter = {
+//   value: 0
+// }
 
 // let percent = $(".percent");
-let precent = document.getElementById("percent")
+// let precent = document.getElementById("percent")
 
 let tl = gsap.timeline({
   defaults: {ease: "power2.out", duration: 2}
@@ -43,8 +47,8 @@ let tl = gsap.timeline({
 
 
 tl
-  .to(counter, 1, {ease: "none", value: 100, onUpdate: () => percent.innerHTML = Math.round(counter.value) + "%"})
-  .to(".percent", 1, {opacity: 0})
+  // .to(counter, 1, {ease: "none", value: 100, onUpdate: () => percent.innerHTML = Math.round(counter.value) + "%"})
+  // .to(".percent", 1, {opacity: 0})
   .staggerFrom($(".hero .paragraph"), 2, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1)
   .staggerFrom($(".content"), 2, {opacity: 0, y: 25, ease: Power1.easeOut}, 0.1, "-=2")
   .to(".preloader", 1, {opacity: 0}, "-=0.5")
